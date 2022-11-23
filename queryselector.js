aea1097193013d6c7514c67b88f4532f3d3e6921
asif-karim100 <asif.karim100@gmail.com>
2022-11-23 08:37:40 +0530

    queryselector.js

---
+++ queryselector.js
@@ -0,0 +1,15 @@
+
+var seconditem=document.querySelector('.list-group-item:nth-child(2)')
+seconditem.style.backgroundColor='green';
+
+var thirditem=document.querySelector('.list-group-item:nth-child(3)')
+thirditem.style.visibility='hidden'
+
+var seconditem=document.querySelectorAll('.list-group-item')
+console.log(seconditem)
+seconditem[2].style.color='green'
+
+var odd = document.querySelectorAll('li:nth-child(odd)')
+for(var i = 0 ;i<odd.length;i++){
+    odd[i].style.backgroundColor = "green"
+}
