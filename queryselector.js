515f505616608c51b89d28e03aa3a11ea9490cac
asif-karim100 <asif.karim100@gmail.com>
2022-11-23 08:41:34 +0530

    queryselector.js

--- queryselector.js
+++ queryselector.js
@@ -8,7 +8,7 @@ thirditem.style.visibility='hidden'
 var seconditem=document.querySelectorAll('.list-group-item')
 console.log(seconditem)
 seconditem[2].style.color='green'
-
+/////////////
 var odd = document.querySelectorAll('li:nth-child(odd)')
 for(var i = 0 ;i<odd.length;i++){
     odd[i].style.backgroundColor = "green"
